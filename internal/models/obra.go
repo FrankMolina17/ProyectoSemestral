c9e72dd9eecb3d242df6510c3766fb7e2ebbb4c1
Carlos Bailon <baioncarlos177@gmail.com>
2026-05-14 01:10:09 -0500

    feat: modelo inicial de Obra e Incidencia - Carlos Andrés Bailón

---
+++ internal/models/obra.go
@@ -0,0 +1,30 @@
+package models
+
+import "time"
+
+type Obra struct {
+	ID                  int       `json:"id" gorm:"primaryKey"`
+	Nombre              string    `json:"nombre"`
+	Descripcion         string    `json:"descripcion"`
+	Ubicacion           string    `json:"ubicacion"`
+	FechaInicio         time.Time `json:"fecha_inicio"`
+	FechaFin            time.Time `json:"fecha_fin"`
+	Estado              string    `json:"estado"`
+	PresupuestoEstimado float64   `json:"presupuesto_estimado"`
+	UserID              int       `json:"user_id"`
+	ProformaID          *int      `json:"proforma_id"`
+}
+
+type Incidencia struct {
+	ID            int     `json:"id" gorm:"primaryKey"`
+	EntidadTipo   string  `json:"entidad_tipo"`
+	EntidadID     int     `json:"entidad_id"`
+	ResponsableID int     `json:"responsable_id"`
+	Titulo        string  `json:"titulo"`
+	Descripcion   string  `json:"descripcion"`
+	Tipo          string  `json:"tipo"`
+	Prioridad     string  `json:"prioridad"`
+	Estado        string  `json:"estado"`
+	ImpactoCosto  float64 `json:"impacto_costo"`
+	ImpactoTiempo int     `json:"impacto_tiempo"`
+}
