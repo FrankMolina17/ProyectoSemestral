688a9da026209ebbf78fdc74b7de7e8e1d51617d
Carlos3B004 <bailoncarlos177@gmail.com>
2026-06-02 20:04:44 -0500

    feat: crear almacenamiento en memoria para Incidencias

---
+++ internal/storage/incidencia_memory.go
@@ -0,0 +1,9 @@
+package storage
+
+import "Sistem-Inte-Gestion-Control-Obras/internal/models"
+
+var Obras []models.Obra
+var ObraIDCounter = 1
+
+var Incidencias []models.Incidencia
+var IncidenciaIDCounter = 1
