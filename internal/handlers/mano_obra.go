5074eb16bb11a46aea000ebeff3c36488edf6805
melana80 <yuyinlan990@gmail.com>
2026-06-08 02:47:46 -0500

    feat: añadir handler GET y POST

---
+++ internal/handlers/mano_obra.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"net/http"
+
+	"Sistem-Inte-Gestion-Control-Obras/internal/models"
+	"Sistem-Inte-Gestion-Control-Obras/internal/storage"
+)
+
+type ManoObraHandler struct{ s *storage.Storage } 
+
+func NewManoObraHandler(s *storage.Storage) *ManoObraHandler { //mano de obra
+	return &ManoObraHandler{s: s}
+}
+
+// GET api/v1/manoobra
+func (h *ManoObraHandler) List(w http.ResponseWriter, r *http.Request) {
+	categoria := r.URL.Query().Get("categoria")
+	list := h.s.ListarManoObra(categoria)
+	respondOK(w, list)
+}
+
+// GET /mano-obra/:id
+func (h *ManoObraHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	id, ok := urlParamID(w, r, "id")
+	if !ok {
+		return
+	}
+	mo, err := h.s.ObtenerManoObraPorID(id)
+	if err != nil {
+		mapStoreError(w, err, "mano_obra", id)
+		return
+	}
+	respondOK(w, mo)
+}
+
+// POST /mano-obra
+func (h *ManoObraHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var in models.ManoObraInput
+	if !decodeJSON(w, r, &in) {
+		return
+	}
+	if err := in.Validate(); err != nil {
+		respondError(w, http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	mo, err := h.s.CrearManoObra(in)
+	if err != nil {
+		mapStoreError(w, err, "mano_obra", 0)
+		return
+	}
+	respondCreated(w, mo, mo.ID)
+}
