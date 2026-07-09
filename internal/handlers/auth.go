df117bbd1c517d23d586bcac352abe62271ca468
FrankMolina17 <franklinmoloina12172004@gmail.com>
2026-07-09 03:35:45 -0500

    Revert "demo: fallo de compilacion CP2"
    This reverts commit c0352a868cba03edf766aff406b247d0d818a324.

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -17,7 +17,7 @@ func (s *ServerC) RegistrarUser(w http.ResponseWriter, r *http.Request) {
 		RespondError(w, http.StatusBadRequest, "Datos invalidos "+err.Error())
 		return
 	}
-	proforma, err := s.Autenticacion.RegistrarUsuario(credenciales.Email, credenciales.Password)
+	usuario, err := s.Autenticacion.RegistrarUsuario(credenciales.Email, credenciales.Password)
 	if err != nil {
 		RespondError(w, StatusDeError(err), err.Error())
 		return
